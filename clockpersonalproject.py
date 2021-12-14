3d349ae766b4e839d0eb568b9041c42374b72a62
Lauren Guthrie <83042306+lrngthr@users.noreply.github.com>
2021-12-13 23:48:40 -0500

    Add files via upload

---
+++ clockpersonalproject.py
@@ -0,0 +1,12 @@
+import sys
+from tkinter import *
+import datetime
+import time
+import winsound
+from PIL import ImageTk, Image
+
+
+
+clock = Tk()
+clock.geometry("400x300")
+start = PhotoImage(file)
